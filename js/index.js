df40fce89c89a2a86aafe856c039ccab90aed453
Daniela Mansilla <dmansill@emeal.nttdata.com>
2024-11-23 19:18:21 -0300

    Feat: base proyecto de salón de fiestas Oli Oli con inicio, servicios y consultas listao sin ser responsivo pero con estilos y diseños básicos

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// HEADER
+document.getElementById("navbar").innerHTML = `
+        <nav class="navbar">
+            <ul>
+                <li class="icono"></li>
+                <li><a href="/index.html">Inicio</a></li>
+                <li><a href="/servicios.html">Servicios</a></li>
+                <li><a href="/reservas.html">Consultas</a></li>
+            </ul>
+        </nav>
+`;
+
+
+// IMAGENES
+document.getElementById("imagenes").innerHTML = `
+    <section class="contenedor-imagenes">
+        <img src="/img/niños2.jpg" alt="Imágen de dos nenas y un nene recostados en un pelotero del salón de 
+        eventos Oli Oli mientrás sonrien.">
+        <img src="/img/pelotero3.jpg" alt="Imágen de una sala sumamente colorida con colores: rojo, blanco, 
+        azul, naranja, amarillo y verde, con varios juegos del salón de eventos Oli Oli.">
+        <img src="/img/pelotero2.jpg" alt="Imágen de una sala sumamente colorida con colores: rojo, blanco, 
+        azul, naranja, amarillo y verde, con varios juegos del salón de eventos Oli Oli">
+    </section>
+`;
+
+
+// FOOTER
+document.getElementById("footer").innerHTML = `
+    <footer>
+        <label> &copy; 2024 - Oli Oli fiestas </label>
+        <div class="contenedor_redes">
+            <a href=""><img class="redes" src="/img/iconos/facebook.png" alt="Icono de la red social Facebook."></a>
+            <a href=""><img class="redes" src="/img/iconos/instagram.png" alt="Icono de la red social Instagram."></a>
+            <a href=""><img class="redes" src="/img/iconos/twitter.png" alt="Icono de la red social X."></a>
+        </div>
+    </footer>
+`;
